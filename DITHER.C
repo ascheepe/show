f0fe33d0474ef08175a4ebdda74fed06f3148085
Axel Scheepers <no@spam.please>
2023-12-16 15:26:07 +0100

    renames

--- DITHER.C
+++ DITHER.C
@@ -26,7 +26,7 @@ struct error {
 	int r, g, b, Y;
 };
 
-static struct error error[2][MAX_IMAGE_WIDTH];
+static struct error err[2][MAX_IMAGE_WIDTH];
 
 /*
  * Clamp a value between 0 and 255, inclusive.
@@ -78,7 +78,7 @@ grayscale_dither(struct bitmap *bmp, int ncolors)
 {
 	int row, col;
 
-	memset(error, 0, sizeof(error));
+	memset(err, 0, sizeof(err));
 	for (row = 0; row < bmp->height - 1; ++row) {
 		size_t ofs = row * bmp->width;
 
@@ -87,23 +87,23 @@ grayscale_dither(struct bitmap *bmp, int ncolors)
 		fflush(stdout);
 		for (col = 1; col < bmp->width - 1; ++col) {
 			struct rgb *color;
-			BYTE old_pixel, new_pixel;
-			int Y_error;
+			BYTE oldpixel, newpixel;
+			int Yerr;
 
 			color = &bmp->palette[bmp->image[ofs + col]];
-			old_pixel = clamp(color_to_mono(color) + error[0][col].Y);
-			new_pixel = (old_pixel * ncolors / 256) * (256 / ncolors);
-			bmp->image[ofs + col] = new_pixel;
+			oldpixel = clamp(color_to_mono(color) + err[0][col].Y);
+			newpixel = (oldpixel * ncolors / 256) * (256 / ncolors);
+			bmp->image[ofs + col] = newpixel;
 
-			Y_error = old_pixel - new_pixel;
-			error[0][col + 1].Y += Y_error * 7 / 16;
-			error[1][col - 1].Y += Y_error * 3 / 16;
-			error[1][col    ].Y += Y_error * 5 / 16;
-			error[1][col + 1].Y += Y_error * 1 / 16;
+			Yerr = oldpixel - newpixel;
+			err[0][col + 1].Y += Yerr * 7 / 16;
+			err[1][col - 1].Y += Yerr * 3 / 16;
+			err[1][col    ].Y += Yerr * 5 / 16;
+			err[1][col + 1].Y += Yerr * 1 / 16;
 		}
 
-		memcpy(&error[0][0], &error[1][0], sizeof(error[0]));
-		memset(&error[1][0], 0, sizeof(error[1]));
+		memcpy(&err[0][0], &err[1][0], sizeof(err[0]));
+		memset(&err[1][0], 0, sizeof(err[1]));
 	}
 }
 
@@ -115,7 +115,7 @@ dither(struct bitmap *bmp, struct rgb *palette, int ncolors)
 {
 	int row, col;
 
-	memset(error, 0, sizeof(error));
+	memset(err, 0, sizeof(err));
 	for (row = 0; row < bmp->height - 1; ++row) {
 		size_t ofs = row * bmp->width;
 
@@ -123,47 +123,47 @@ dither(struct bitmap *bmp, struct rgb *palette, int ncolors)
 		printf("D:%3d%%\r", row * 100 / bmp->height);
 		fflush(stdout);
 		for (col = 1; col < bmp->width - 1; ++col) {
-			struct rgb old_pixel, new_pixel;
+			struct rgb oldpixel, newpixel;
 			struct rgb *color;
-			int red_error, green_error, blue_error;
+			int rerr, gerr, berr;
 			int palidx;
 
 			color = &bmp->palette[bmp->image[ofs + col]];
-			old_pixel.r = clamp(color->r + error[0][col].r);
-			old_pixel.g = clamp(color->g + error[0][col].g);
-			old_pixel.b = clamp(color->b + error[0][col].b);
+			oldpixel.r = clamp(color->r + err[0][col].r);
+			oldpixel.g = clamp(color->g + err[0][col].g);
+			oldpixel.b = clamp(color->b + err[0][col].b);
 
-			palidx = pick(&old_pixel, palette, ncolors);
+			palidx = pick(&oldpixel, palette, ncolors);
 			bmp->image[ofs + col] = palidx;
 
 			color = &palette[palidx];
-			new_pixel.r = color->r;
-			new_pixel.g = color->g;
-			new_pixel.b = color->b;
+			newpixel.r = color->r;
+			newpixel.g = color->g;
+			newpixel.b = color->b;
 
-			red_error   = old_pixel.r - new_pixel.r;
-			green_error = old_pixel.g - new_pixel.g;
-			blue_error  = old_pixel.b - new_pixel.b;
+			rerr = oldpixel.r - newpixel.r;
+			gerr = oldpixel.g - newpixel.g;
+			berr = oldpixel.b - newpixel.b;
 
-			error[0][col + 1].r += red_error   * 7 / 16;
-			error[0][col + 1].g += green_error * 7 / 16;
-			error[0][col + 1].b += blue_error  * 7 / 16;
+			err[0][col + 1].r += rerr * 7 / 16;
+			err[0][col + 1].g += gerr * 7 / 16;
+			err[0][col + 1].b += berr * 7 / 16;
 
-			error[1][col - 1].r += red_error   * 3 / 16;
-			error[1][col - 1].g += green_error * 3 / 16;
-			error[1][col - 1].b += blue_error  * 3 / 16;
+			err[1][col - 1].r += rerr * 3 / 16;
+			err[1][col - 1].g += gerr * 3 / 16;
+			err[1][col - 1].b += berr * 3 / 16;
 
-			error[1][col    ].r += red_error   * 5 / 16;
-			error[1][col    ].g += green_error * 5 / 16;
-			error[1][col    ].b += blue_error  * 5 / 16;
+			err[1][col    ].r += rerr * 5 / 16;
+			err[1][col    ].g += gerr * 5 / 16;
+			err[1][col    ].b += berr * 5 / 16;
 
-			error[1][col + 1].r   += red_error   * 1 / 16;
-			error[1][col + 1].g += green_error * 1 / 16;
-			error[1][col + 1].b  += blue_error  * 1 / 16;
+			err[1][col + 1].r += rerr * 1 / 16;
+			err[1][col + 1].g += gerr * 1 / 16;
+			err[1][col + 1].b += berr * 1 / 16;
 		}
 
-		memcpy(&error[0][0], &error[1][0], sizeof(error[0]));
-		memset(&error[1][0], 0, sizeof(error[1]));
+		memcpy(&err[0][0], &err[1][0], sizeof(err[0]));
+		memset(&err[1][0], 0, sizeof(err[1]));
 	}
 }
 
@@ -183,8 +183,8 @@ ordered_dither(struct bitmap *bmp, struct rgb *palette, int ncolors)
 	int row, col;
 
 	for (row = 0; row < bmp->height; ++row) {
-		BYTE Mrow = row & 7;
 		size_t ofs = row * bmp->width;
+		BYTE Mrow = row & 7;
 
 		maybe_exit();
 		printf("D:%3d%%\r", row * 100 / bmp->height);
@@ -193,14 +193,14 @@ ordered_dither(struct bitmap *bmp, struct rgb *palette, int ncolors)
 		for (col = 0; col < bmp->width; ++col) {
 			int palidx = bmp->image[ofs + col];
 			struct rgb *color = &bmp->palette[palidx];
-			struct rgb new_color;
+			struct rgb newcolor;
 			BYTE Mcol = col & 7;
 
-			new_color.r = color->r > (4 * M[Mrow][Mcol]) ? 255 : 0;
-			new_color.g = color->g > (4 * M[Mrow][Mcol]) ? 255 : 0;
-			new_color.b = color->b > (4 * M[Mrow][Mcol]) ? 255 : 0;
+			newcolor.r = color->r > (4 * M[Mrow][Mcol]) ? 255 : 0;
+			newcolor.g = color->g > (4 * M[Mrow][Mcol]) ? 255 : 0;
+			newcolor.b = color->b > (4 * M[Mrow][Mcol]) ? 255 : 0;
 
-			palidx = pick(&new_color, palette, ncolors);
+			palidx = pick(&newcolor, palette, ncolors);
 			bmp->image[ofs + col] = palidx;
 		}
 	}
