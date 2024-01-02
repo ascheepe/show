32b394d1389bbd9d75254163831db5164933516b
Axel Scheepers <no@spam.please>
2024-01-02 20:37:55 +0100

    adjust copyright

--- EGA.C
+++ EGA.C
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2020-2022 Axel Scheepers
+ * Copyright (c) 2020-2024 Axel Scheepers
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
